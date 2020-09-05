3efb188b8a81a896dca9d83be77e5e277e9801df
Mayank <mayankverma76342@gmail.com>
2020-09-05 14:58:06 -0700

    initial push
    initial push

---
+++ mayank-titok-clone/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello from front end</h1>
+    </div>
+  );
+}
+export default App;
